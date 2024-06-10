4f02f1a22e5d941a145ac879844c3b6ab169c7dc
Tooster <5300963+T3sT3ro@users.noreply.github.com>
2024-06-10 09:33:20 +0200

    Improved Sphere SDF controller gizmo
    - it now rotates with the transform
    - fixed NPE when property field was accessed when inspector didn't exist
    
    Signed-off-by: Tooster <5300963+T3sT3ro@users.noreply.github.com>

--- Packages/me.tooster.sdf/Editor/Controllers/Editors/PrimitiveControllers/SdfSphereControllerEditor.cs
+++ Packages/me.tooster.sdf/Editor/Controllers/Editors/PrimitiveControllers/SdfSphereControllerEditor.cs
@@ -2,27 +2,32 @@ using me.tooster.sdf.Editor.Controllers.SDF;
 using me.tooster.sdf.Editor.Controllers.SDF.Primitives;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.UIElements;
 namespace me.tooster.sdf.Editor.Controllers.Editors.PrimitiveControllers {
     [CustomEditor(typeof(SdfSphereController), true)]
     class SdfSphereControllerEditor : SdfPrimitiveControllerEditor {
         SerializedProperty radiusProperty;
 
+        protected override void OnEnable() {
+            base.OnEnable();
+            // FIXME: don't depend on hidden internal property, maybe use source generators, expressions or expose an API 
+            radiusProperty = serializedObject.FindProperty("radius");
+        }
+
         protected override void OnSceneGUI() {
             base.OnSceneGUI();
             var controller = (SdfSphereController)target;
-            using var scope = new EditorGUI.ChangeCheckScope();
-            var radius = Handles.RadiusHandle(Quaternion.identity, controller.transform.position, controller.Radius);
-            if (!scope.changed) return;
+            var tr = controller.transform;
 
-            controller.Radius = radiusProperty.floatValue = radius;
-            serializedObject.ApplyModifiedProperties();
-        }
+            using (new Handles.DrawingScope(Matrix4x4.TRS(tr.position, tr.rotation, Vector3.one))) {
+                using (var radiusCheck = new EditorGUI.ChangeCheckScope()) {
+                    var radius = Handles.RadiusHandle(Quaternion.identity, controller.transform.position, controller.Radius);
 
-        public override VisualElement CreateInspectorGUI() {
-            // FIXME: don't depend on hidden internal property, maybe use source generators, expressions or expose an API 
-            radiusProperty = serializedObject.FindProperty("radius");
-            return base.CreateInspectorGUI();
+                    if (!radiusCheck.changed) return;
+
+                    controller.Radius = radiusProperty.floatValue = radius;
+                    serializedObject.ApplyModifiedProperties();
+                }
+            }
         }
 
 
